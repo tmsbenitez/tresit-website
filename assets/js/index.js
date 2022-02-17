9eff9aecab7e8e704cde36a27190dfa872a05883
Tomasb <tomasezequiel.benitez@gmail.com>
2022-02-17 15:02:51 -0300

    tresit-v0.3.2
    mobile menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,27 @@
-window.addEventListener("scroll", function() {
-  var header = document.querySelector("header")
-  header.classList.toggle("abajo", window.scrollY > 50)
-})
+// HEADER WHITE
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("abajo", window.scrollY > 50);
+});
+
+// MENU DESPLEGABLE
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+
+const menuLinks = document.querySelectorAll(".menu");
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", function () {
+    menuLink.classList.remove("menu_opened");
+  });
+});
